08f8e056aff511b263dfa84ef77b8160ccc0fdad
talitamarques <talitadelania123@gmail.com>
2023-11-02 19:43:37 -0300

    add files

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask("__name__", template_folder='templates')
+
+@app.route("/")
+@app.route("/home")
+def homepage():
+    return render_template("home.html")
+
+@app.route("/writings")
+def quem():
+    return render_template("writings.html")
+
+@app.route("/pictures")
+def contato():
+    return render_template("pictures.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
